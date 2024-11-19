7709107f6b849a8bb5cf949f8510338b7883cfc0
ignimkk <kmingi98@gmail.com>
2024-11-19 21:06:40 +0900

    modified urdf

--- ribot_description/launch/display_ribot.launch.py
+++ ribot_description/launch/display_ribot.launch.py
@@ -1,62 +1,46 @@
-from ament_index_python.packages import get_package_share_path
+import os
+from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.conditions import IfCondition, UnlessCondition
 from launch.substitutions import Command, LaunchConfiguration
 
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
 
+
 def generate_launch_description():
-    ribot_description_path = get_package_share_path('ribot_description')
-    default_model_path = ribot_description_path / 'urdf/ribot.urdf'
-    default_rviz_config_path = ribot_description_path / 'rviz/ribot.rviz'  # Add your RViz config file if exists
+    ribot_description_dir = get_package_share_directory("ribot_description")
 
-    gui_arg = DeclareLaunchArgument(name='gui', default_value='true', choices=['true', 'false'],
-                                    description='Flag to enable joint_state_publisher_gui')
-    model_arg = DeclareLaunchArgument(name='model', default_value=str(default_model_path),
-                                      description='Absolute path to robot urdf file')
-    rviz_arg = DeclareLaunchArgument(name='rvizconfig', default_value=str(default_rviz_config_path),
-                                     description='Absolute path to rviz config file')
+    model_arg = DeclareLaunchArgument(name="model", default_value=os.path.join(
+                                        ribot_description_dir, "urdf", "ribot.urdf.xacro"
+                                        ),
+                                      description="Absolute path to robot urdf file")
 
-    robot_description = ParameterValue(Command(['xacro ', LaunchConfiguration('model')]),
+    robot_description = ParameterValue(Command(["xacro ", LaunchConfiguration("model")]),
                                        value_type=str)
 
-    # Node to publish the robot state from URDF
     robot_state_publisher_node = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        parameters=[{'robot_description': robot_description}]
-    )
-
-    # Depending on gui parameter, either launch joint_state_publisher or joint_state_publisher_gui
-    joint_state_publisher_node = Node(
-        package='joint_state_publisher',
-        executable='joint_state_publisher',
-        condition=UnlessCondition(LaunchConfiguration('gui'))
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[{"robot_description": robot_description}]
     )
 
     joint_state_publisher_gui_node = Node(
-        package='joint_state_publisher_gui',
-        executable='joint_state_publisher_gui',
-        condition=IfCondition(LaunchConfiguration('gui'))
+        package="joint_state_publisher_gui",
+        executable="joint_state_publisher_gui"
     )
 
-    # RViz node
     rviz_node = Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz2',
-        output='screen',
-        arguments=['-d', LaunchConfiguration('rvizconfig')],
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="screen",
+        arguments=["-d", os.path.join(ribot_description_dir, "rviz", "display.rviz")],
     )
 
     return LaunchDescription([
-        gui_arg,
         model_arg,
-        rviz_arg,
-        joint_state_publisher_node,
         joint_state_publisher_gui_node,
         robot_state_publisher_node,
         rviz_node
